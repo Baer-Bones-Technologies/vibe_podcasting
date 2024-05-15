b4d08880d0f7475c689c0ec9b696700c30ceab5f
Jamie Williams <66984547+BaerBonesTechnology@users.noreply.github.com>
2024-05-15 08:55:02 -0400

    registration screen skeleton

--- lib/pages/auth/login.dart
+++ lib/pages/auth/login.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:vibe_podcasting/constants/strings.dart';
-import 'package:vibe_podcasting/pages/auth/auth_screen.dart';
-import 'package:vibe_podcasting/pages/templates/registration.dart';
-import 'package:vibe_podcasting/providers/auth_provider.dart';
+
+import '../../constants/strings.dart';
+import '../../providers/auth_provider.dart';
+import '../templates/registration.dart';
+import 'auth_screen.dart';
+import 'register.dart';
 
 class LoginScreen extends ConsumerWidget {
   LoginScreen({super.key});
@@ -19,112 +21,82 @@ class LoginScreen extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final authController = ref.watch(authManagerProvider);
 
-    return RegistrationScaffold(
-        form: Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(loginLabel,
-            style: GoogleFonts.poppins(
-                textStyle: Theme.of(context).textTheme.displayLarge,
-                color: Colors.black)),
-        Text(
-          loginTagLine,
-          style: Theme.of(context).textTheme.bodySmall,
-        ),
-        const SizedBox(height: 32),
-        SizedBox(
-          width: MediaQuery.of(context).size.width * 0.8,
-          child: TextField(
-            controller: _emailController,
-            decoration: InputDecoration(
-              labelText: 'Email',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
+    return SingleChildScrollView(
+      controller: ScrollController()
+        ..addListener(() => FocusScope.of(context).unfocus()),
+      child: RegistrationScaffold(
+          form: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(loginLabel,
+              style: GoogleFonts.poppins(
+                  textStyle: Theme.of(context).textTheme.displayLarge,
+                  color: Colors.black)),
+          Text(
+            loginTagLine,
+            style: Theme.of(context).textTheme.bodySmall,
           ),
-        ),
-        const SizedBox(height: 16),
-        SizedBox(
-          width: MediaQuery.of(context).size.width * 0.8,
-          child: TextField(
-            controller: _passwordController,
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: 'Password',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-          ),
-        ),
-        const SizedBox(height: 16),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            TextButton(
-              onPressed: () {
-                context.push(AuthScreen.routeLocation);
-              },
-              child: const Text('Forgot Password?'),
-            ),
-          ],
-        ),
-        const Spacer(),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                final email = _emailController.value.text;
-                final password = _passwordController.value.text;
-                try {
-                  authController.signInWithEmailAndPassword(
-                      email: email, password: password);
-                } catch (e) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text(e.toString()),
-                      backgroundColor: Theme.of(context).colorScheme.error,
-                    ),
-                  );
-                }
-              },
-              style: ButtonStyle(
-                minimumSize: WidgetStateProperty.all(
-                  Size(
-                    MediaQuery.of(context).size.width * 0.8,
-                    50,
-                  ),
+          const SizedBox(height: 32),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
                 ),
               ),
-              child: const Text(loginLabel),
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(vertical: 16),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Divider(
-                    thickness: 2,
-                    color: Colors.blue,
-                    indent: 10,
-                    endIndent: 10,
-                  ),
-                  Text('or',),
-                  Divider(
-                    thickness: 2,
-                    color: Colors.blue,
-                    indent: 10,
-                    endIndent: 10,
-                  ),
-                ],
+          ),
+          const SizedBox(height: 16),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.8,
+            child: TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
               ),
             ),
-            OutlinedButton(
-                onPressed: () {},
+          ),
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: () {
+                  context.push(AuthScreen.routeLocation);
+                },
+                child: const Text('Forgot Password?'),
+              ),
+            ],
+          ),
+          const Spacer(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  final email = _emailController.value.text;
+                  final password = _passwordController.value.text;
+                  try {
+                    authController.signInWithEmailAndPassword(
+                        email: email, password: password);
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(e.toString()),
+                        backgroundColor: Theme.of(context).colorScheme.error,
+                      ),
+                    );
+                  }
+                },
                 style: ButtonStyle(
                   minimumSize: WidgetStateProperty.all(
                     Size(
@@ -133,35 +105,71 @@ class LoginScreen extends ConsumerWidget {
                     ),
                   ),
                 ),
-                child: const Text('Sign in with Google')),
-            const SizedBox(height: 16),
-            OutlinedButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  minimumSize: WidgetStateProperty.all(
-                    Size(
-                      MediaQuery.of(context).size.width * 0.8,
-                      50,
+                child: const Text(loginLabel),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Divider(
+                      thickness: 2,
+                      color: Colors.blue,
+                      indent: 10,
+                      endIndent: 10,
+                    ),
+                    Text(
+                      'or',
+                    ),
+                    Divider(
+                      thickness: 2,
+                      color: Colors.blue,
+                      indent: 10,
+                      endIndent: 10,
+                    ),
+                  ],
+                ),
+              ),
+              OutlinedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    minimumSize: WidgetStateProperty.all(
+                      Size(
+                        MediaQuery.of(context).size.width * 0.8,
+                        50,
+                      ),
                     ),
                   ),
-                ),
-                child: const Text('Sign in with Facebook'))
-          ],
-        ),
-        const SizedBox(height: 16),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Don\'t have an account?'),
-            TextButton(
-              onPressed: () {
-                context.push(AuthScreen.routeLocation);
-              },
-              child: const Text('Register'),
-            ),
-          ],
-        ),
-      ],
-    ));
+                  child: const Text('Sign in with Google')),
+              const SizedBox(height: 16),
+              OutlinedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    minimumSize: WidgetStateProperty.all(
+                      Size(
+                        MediaQuery.of(context).size.width * 0.8,
+                        50,
+                      ),
+                    ),
+                  ),
+                  child: const Text('Sign in with Facebook'))
+            ],
+          ),
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text('Don\'t have an account?'),
+              TextButton(
+                onPressed: () {
+                  context.push(RegisterScreen.routeLocation);
+                },
+                child: const Text('Register'),
+              ),
+            ],
+          ),
+        ],
+      )),
+    );
   }
 }
--- lib/pages/auth/register.dart
+++ lib/pages/auth/register.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:vibe_podcasting/pages/templates/registration.dart';
 
 import 'auth_screen.dart';
@@ -14,12 +13,148 @@ class RegisterScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    registrationScreen({Map<String, dynamic>? args}) =>
+        ref.watch(registrationScreenProvider(args: args));
+
     return RegistrationScaffold(
-      form: BackButton(onPressed: () {
-        if (context.canPop()) {
-          context.pop();
-        }
-      }),
-    );
+        form: Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Register',
+            style: GoogleFonts.poppins(
+                textStyle: Theme.of(context).textTheme.displayLarge,
+                color: Colors.black)),
+        Text(
+          'Create an account to get started',
+          style: Theme.of(context).textTheme.bodySmall,
+        ),
+        const SizedBox(height: 32),
+        registrationScreen(args: {
+          'onSubmit': () {},
+        })
+      ],
+    ));
   }
+}
+
+registrationScreenProvider({Map<String, dynamic>? args}) =>
+    Provider<Widget>((ref) => initialRegistrationScreen(ref,
+        onSubmit: args?['onSubmit'] as void Function()));
+
+Widget initialRegistrationScreen(ProviderRef ref, {Function? onSubmit}) {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.start,
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      SizedBox(
+        width: double.infinity,
+        child: TextField(
+          controller: emailController,
+          decoration: InputDecoration(
+            labelText: 'Email',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+      ),
+      const SizedBox(height: 16),
+      SizedBox(
+        width: double.infinity,
+        child: TextField(
+          controller: passwordController,
+          obscureText: true,
+          decoration: InputDecoration(
+            labelText: 'Password',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+      ),
+      const SizedBox(height: 16),
+      SizedBox(
+        width: double.infinity,
+        child: TextField(
+          controller: confirmPasswordController,
+          obscureText: true,
+          decoration: InputDecoration(
+            labelText: 'Confirm Password',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+      ),
+      const SizedBox(height: 16),
+      ElevatedButton(
+        onPressed: onSubmit as void Function()? ?? () {},
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(double.infinity, 50),
+        ),
+        child: const Text('Register'),
+      ),
+      const SizedBox(height: 16),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          const Text('Already have an account?'),
+          TextButton(
+            onPressed: () {},
+            child: const Text('Login'),
+          ),
+        ],
+      ),
+      const SizedBox(height: 16),
+      const Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Divider(
+            color: Colors.black,
+            height: 20,
+            thickness: 2,
+            indent: 0,
+            endIndent: 0,
+          ),
+          Text('OR'),
+          Divider(
+            color: Colors.black,
+            height: 20,
+            thickness: 2,
+            indent: 0,
+            endIndent: 0,
+          ),
+        ],
+      ),
+      const SizedBox(height: 16),
+      OutlinedButton(
+          onPressed: () {},
+          style: ButtonStyle(
+            minimumSize: WidgetStateProperty.all(
+              const Size(
+                double.infinity,
+                50,
+              ),
+            ),
+          ),
+          child: const Text('Sign up with Google')),
+      const SizedBox(height: 16),
+      OutlinedButton(
+          onPressed: () {},
+          style: ButtonStyle(
+            minimumSize: WidgetStateProperty.all(
+              const Size(
+                double.infinity,
+                50,
+              ),
+            ),
+          ),
+          child: const Text('Sign up with Facebook'))
+    ],
+  );
 }
